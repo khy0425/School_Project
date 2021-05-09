a7744ad1e6e7928ba35394fc5c0a8bad8e774c31
khy0425 <from_ap@naver.com>
2021-05-09 22:05:58 +0900

    코드 완성

--- C/2017620051_김호영_maxmin.c
+++ C/2017620051_김호영_maxmin.c
@@ -22,43 +22,28 @@ int main()
         {
             printf("%d번째 제품의 가격을 입력하시오: ", i + 1);
             scanf("%d", &price);
-            // max = price;
         } while (price < 1000 || price > 10000);
+
         if (price < min)
         {
             min = price;
             sum = sum + price;
-            printf("최대값 : %d\n", max);
-            printf("최소값 : %d\n", min);
+        }
+        else if (price >= min)
+        {
+            sum = sum + price;
         }
         else if (price > max)
         {
-            printf("여기 작동 되요 값이 max 보다 커요!\n");
             max = price;
             sum = sum + price;
-            printf("최대값 : %d\n", max);
-            printf("최소값 : %d\n", min);
         }
-
-        else if (price >= min)
-        {
-            printf("여기 작동 되요!\n");
-            sum = sum + price;
-            printf("최대값 : %d\n", max);
-            printf("최소값 : %d\n", min);
-        }
-
         else if (price <= max)
         {
-            printf("여기 작동 되요! 값이 max 보다 크거나 같아요 !\n");
             sum = sum + price;
-            printf("최대값 : %d\n", max);
-            printf("최소값 : %d\n", min);
         }
     }
     avg = sum / goods;
 
     printf("최대값: %d 최소값: %d 평균값: %.0f\n", max, min, avg);
-
-    return 0;
 }
